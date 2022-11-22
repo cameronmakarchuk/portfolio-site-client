9a676f9e9ef9177b7904f26c33d5d3f57b584a9d
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-22 17:39:15 -0500

    Add profile picture to PageHeader. Setup initial files for MainHero component

---
+++ src/components/MainHero/MainHero.tsx
@@ -0,0 +1,12 @@
+import "./MainHero.scss";
+
+export default function MainHero(): JSX.Element {
+  return (
+    <section className="main-hero">
+      <h1 className="main-hero__title">
+        Hi, I'm Cameron. I'm a software developer, fitness entreprenuer, and
+        travel enthusiast.
+      </h1>
+    </section>
+  );
+}
--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,30 +1,35 @@
 import "./PageHeader.scss";
-import darkToggleIcon from "../../assets/icons/dark_mode_FILL0_wght400_GRAD0_opsz48.svg"
-import lightToggleIcon from "../../assets/icons/light_mode_FILL0_wght400_GRAD0_opsz48.svg"
-import {NavLink} from "react-router-dom";
-
-
+import darkToggleIcon from "../../assets/icons/dark_mode_FILL0_wght400_GRAD0_opsz48.svg";
+import _lightToggleIcon from "../../assets/icons/light_mode_FILL0_wght400_GRAD0_opsz48.svg";
+import camProfilePic from "../../assets/images/cam-headshot.jpg";
+import { NavLink } from "react-router-dom";
 
 export default function PageHeader(): JSX.Element {
-    
-    return (
-        <header className="page-header">
-            <nav className="page-header__nav">
-                <ul className="page-header__nav-list">
-                <NavLink to="" className="page-header__nav-link">
-                    <li className="page-header__nav-item">About</li>
-                        </NavLink>
-                        <NavLink to="" className="page-header__nav-link">
-                    <li className="page-header__nav-item">Projects</li>
-                    </NavLink>
-                    <NavLink to="" className="page-header__nav-link">
-                    <li className="page-header__nav-item">Contact</li>
-                    </NavLink>
-                </ul>
-
-               <img src={darkToggleIcon} alt="light dark toggle" className="page-header__light-dark-toggle" />
-
-            </nav>
-        </header>
-    )
+  return (
+    <header className="page-header">
+      <img
+        src={camProfilePic}
+        alt="Cameron profile"
+        className="page-header__profile-img"
+      />
+      <nav className="page-header__nav">
+        <ul className="page-header__nav-list">
+          <NavLink to="" className="page-header__nav-link">
+            <li className="page-header__nav-item">About</li>
+          </NavLink>
+          <NavLink to="" className="page-header__nav-link">
+            <li className="page-header__nav-item">Projects</li>
+          </NavLink>
+          <NavLink to="" className="page-header__nav-link">
+            <li className="page-header__nav-item">Contact</li>
+          </NavLink>
+        </ul>
+      </nav>
+      <img
+        src={darkToggleIcon}
+        alt="light dark toggle"
+        className="page-header__light-dark-toggle"
+      />
+    </header>
+  );
 }
