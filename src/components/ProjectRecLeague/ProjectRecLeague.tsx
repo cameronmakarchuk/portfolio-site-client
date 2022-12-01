b2caa92bb0eef734289b612b0b87d5ac9a7e3419
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-30 23:32:24 -0500

    Refactor ProjectRecLeague title, subtitle, mockups into ProjectHero component. Create RecLeagueDetails and ProjectHeroProps interface for reusability

--- src/components/ProjectRecLeague/ProjectRecLeague.tsx
+++ src/components/ProjectRecLeague/ProjectRecLeague.tsx
@@ -2,31 +2,28 @@ import React from "react";
 import recleagueiPhone from "../../assets/images/recleague-iphone.png";
 import recleagueAddUserIphone from "../../assets/images/recleague-adduser-iphone.png";
 import recleagueProfileIphone from "../../assets/images/recleague-profile-iphone.png";
+import ProjectHero from "../ProjectHero/ProjectHero";
+
+export interface RecLeagueDetails {
+  title: string;
+  position: string;
+  responsibilities: string;
+  date: string;
+  mockups: string[];
+}
 
 export default function ProjectRecLeague(): JSX.Element {
+  const recleagueDetails: RecLeagueDetails = {
+    title: "RecLeague",
+    position: "Creator",
+    responsibilities: "UI/UX, Development",
+    date: "November 2022",
+    mockups: [recleagueiPhone, recleagueAddUserIphone, recleagueProfileIphone],
+  };
+
   return (
     <section className="project">
-      <h2 className="project__title">RecLeague</h2>
-      <h3 className="project__subtitle">
-        Creator | UI/UX, Development | November 2022
-      </h3>
-      <div className="project__img-container">
-        <img
-          src={recleagueiPhone}
-          alt="recleague iphone mockup"
-          className="project__img-mockup"
-        />
-        <img
-          src={recleagueAddUserIphone}
-          alt="recleague iphone mockup"
-          className="project__img-mockup"
-        />
-        <img
-          src={recleagueProfileIphone}
-          alt="recleague iphone mockup"
-          className="project__img-mockup"
-        />
-      </div>
+      <ProjectHero recleagueDetails={recleagueDetails} />
     </section>
   );
 }
