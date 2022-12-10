19c9170fafba0d491f29d6b335e7dd2e21706289
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-12-10 11:43:47 -0500

    Add links to connect icons

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -12,36 +12,76 @@ export default function ContactPage(): JSX.Element {
       <h2 className="contact-page__title">Connect With Cameron</h2>
 
       <div className="contact-page__connect-wrapper">
-        <img
-          src={emailLogo}
-          alt="Email Cameron"
-          className="contact-page__connect-icon"
-        />
-        <img
-          src={linkedinLogo}
-          alt="Cameron LinkedIn"
-          className="contact-page__connect-icon"
-        />
-        <img
-          src={twitterLogo}
-          alt="Cameron Twitter"
-          className="contact-page__connect-icon"
-        />
-        <img
-          src={instagramLogo}
-          alt="Cameron Instagram"
-          className="contact-page__connect-icon"
-        />
-        <img
-          src={facebookLogo}
-          alt="Cameron Facebook"
-          className="contact-page__connect-icon"
-        />
-        <img
-          src={githubLogo}
-          alt="Cameron GitHub"
-          className="contact-page__connect-icon"
-        />
+        <a
+          href="mailto:cmakarchuk@gmail.com"
+          className="contact-page__connect-link"
+        >
+          <img
+            src={emailLogo}
+            alt="Email Cameron"
+            className="contact-page__connect-icon"
+          />
+        </a>
+        <a
+          href="https://linkedin.com/in/cameronmakarchuk"
+          className="contact-page__connect-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={linkedinLogo}
+            alt="Cameron LinkedIn"
+            className="contact-page__connect-icon"
+          />
+        </a>
+        <a
+          href="https://twitter.com/cmakarchuk"
+          className="contact-page__connect-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={twitterLogo}
+            alt="Cameron Twitter"
+            className="contact-page__connect-icon"
+          />
+        </a>
+        <a
+          href="https://instagram.com/cameronmakarchuk"
+          className="contact-page__connect-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={instagramLogo}
+            alt="Cameron Instagram"
+            className="contact-page__connect-icon"
+          />
+        </a>
+        <a
+          href="https://facebook.com/cmakarchuk"
+          className="contact-page__connect-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={facebookLogo}
+            alt="Cameron Facebook"
+            className="contact-page__connect-icon"
+          />
+        </a>
+        <a
+          href="https://github.com/cameronmakarchuk"
+          className="contact-page__connect-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={githubLogo}
+            alt="Cameron GitHub"
+            className="contact-page__connect-icon"
+          />
+        </a>
       </div>
     </section>
   );
