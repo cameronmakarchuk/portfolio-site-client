eb3b7bb5f95af8a0c4d2f30105fb269736135c66
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-12-13 22:12:03 -0500

    Add desktop styles for MainHero

--- src/components/MainHero/MainHero.tsx
+++ src/components/MainHero/MainHero.tsx
@@ -14,61 +14,63 @@ export default function MainHero(): JSX.Element {
         alt="Cameron profile"
         className="main-hero__profile-img"
       />
-      <h1 className="main-hero__title">
-        Software developer, fitness entreprenuer.
-      </h1>
-      <p className="main-hero__intro">
-        Hi. I'm Cameron, a full-stack software developer and entreprenuer based
-        in Toronto, Ontario, Canada. I spent 15 years in the fitness industry
-        helping 1000's of people become the strongest, healthiest version of
-        themselves. Now I'm leaning into my love of tech through software
-        development.
-      </p>
-      <div className="main-hero__social-icons">
-        <a
-          href="https://twitter.com/cmakarchuk"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={twitterLogo}
-            alt="twitter icon"
-            className="main-hero__social-icon"
-          />
-        </a>
-        <a
-          href="https://instagram.com/cameronmakarchuk"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={instagramLogo}
-            alt="instagram icon"
-            className="main-hero__social-icon"
-          />
-        </a>
-        <a
-          href="https://linkedin.com/in/cameronmakarchuk"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={linkedinLogo}
-            alt="linkedin icon"
-            className="main-hero__social-icon"
-          />
-        </a>
-        <a
-          href="https://github.com/cameronmakarchuk"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={githubLogo}
-            alt="github icon"
-            className="main-hero__social-icon"
-          />
-        </a>
+      <div className="main-hero__profile-wrapper">
+        <h1 className="main-hero__title">
+          Software developer, fitness entreprenuer.
+        </h1>
+        <p className="main-hero__intro">
+          Hi. I'm Cameron, a full-stack software developer and entreprenuer
+          based in Toronto, Ontario, Canada. I spent 15 years in the fitness
+          industry helping 1000's of people become the strongest, healthiest
+          version of themselves. Now I'm leaning into my love of tech through
+          software development.
+        </p>
+        <div className="main-hero__social-icons">
+          <a
+            href="https://twitter.com/cmakarchuk"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={twitterLogo}
+              alt="twitter icon"
+              className="main-hero__social-icon"
+            />
+          </a>
+          <a
+            href="https://instagram.com/cameronmakarchuk"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={instagramLogo}
+              alt="instagram icon"
+              className="main-hero__social-icon"
+            />
+          </a>
+          <a
+            href="https://linkedin.com/in/cameronmakarchuk"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={linkedinLogo}
+              alt="linkedin icon"
+              className="main-hero__social-icon"
+            />
+          </a>
+          <a
+            href="https://github.com/cameronmakarchuk"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={githubLogo}
+              alt="github icon"
+              className="main-hero__social-icon"
+            />
+          </a>
+        </div>
       </div>
     </section>
   );
