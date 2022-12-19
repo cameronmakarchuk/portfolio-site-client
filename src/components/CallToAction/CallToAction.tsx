fcef097dc0ae013fc51b110cc8ddf47cf4a2bdee
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-12-19 16:50:37 -0500

    Update text on CallToAction.tsx

--- src/components/CallToAction/CallToAction.tsx
+++ src/components/CallToAction/CallToAction.tsx
@@ -5,11 +5,8 @@ export default function CallToAction(): JSX.Element {
   return (
     <section className="cta">
       <h2 className="cta__title">
-        I occassionally take on freelance opportunities.
-      </h2>
-      <p className="cta__description">
         Need some help bringing an exciting project to life?
-      </p>
+      </h2>
       <p className="cta__description">
         Send me a message and{" "}
         <a
