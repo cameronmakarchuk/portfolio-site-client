8a988037a9d1e48187b4d2a0a4b3857875c45e08
Cameron Makarchuk <cmakarchuk@gmail.com>
2024-10-27 16:58:04 -0400

    feat: refactor pageheader, adjust main-hero

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,31 +1,62 @@
-import './PageHeader.scss';
+import "./PageHeader.scss";
+import { useState } from "react";
 // import darkToggleIcon from "../../assets/icons/dark_mode_FILL0_wght400_GRAD0_opsz48.svg";
 // import _lightToggleIcon from "../../assets/icons/light_mode_FILL0_wght400_GRAD0_opsz48.svg";
-import { NavLink } from 'react-router-dom';
-import cmSignature from '../../assets/icons/cm-signature-black.svg';
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function PageHeader(): JSX.Element {
+	const location = useLocation();
+	const [active, setActive] = useState<string>(location.pathname);
 	return (
-		<header className='page-header'>
-			<NavLink to='/' className='page-header__nav-link'>
-				<img
-					src={cmSignature}
-					alt='Cameron Makarchuk Signature'
-					className='page-header__logo'
-				/>
-			</NavLink>
-			<nav className='page-header__nav'>
-				<ul className='page-header__nav-list'>
-					<NavLink to='/' className='page-header__nav-link'>
-						<li className='page-header__nav-item'>Home</li>
+		<header className="page-header">
+			<nav className="page-header__nav">
+				<ul className="page-header__nav-list">
+					<NavLink
+						to="/"
+						className="page-header__nav-link"
+						onClick={() => setActive("/")}
+					>
+						<li
+							className={
+								active === "/"
+									? "page-header__nav-item page-header__nav-item--active"
+									: "page-header__nav-item"
+							}
+						>
+							Home
+						</li>
 					</NavLink>
 
-					<NavLink to='/about' className='page-header__nav-link'>
-						<li className='page-header__nav-item'>About</li>
+					<NavLink
+						to="/about"
+						className="page-header__nav-link"
+						onClick={() => setActive("/about")}
+					>
+						<li
+							className={
+								active === "/about"
+									? "page-header__nav-item page-header__nav-item--active"
+									: "page-header__nav-item"
+							}
+						>
+							About
+						</li>
 					</NavLink>
 
-					<NavLink to='/contact' className='page-header__nav-link'>
-						<li className='page-header__nav-item'>Contact</li>
+					<NavLink
+						to="/contact"
+						className="page-header__nav-link"
+						onClick={() => setActive("/contact")}
+					>
+						<li
+							className={
+								active === "/contact"
+									? "page-header__nav-item page-header__nav-item--active"
+									: "page-header__nav-item"
+							}
+						>
+							Contact
+						</li>
 					</NavLink>
 				</ul>
 			</nav>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,11 @@
-import './HomePage.scss';
-import MainHero from '../../components/MainHero/MainHero';
-import Projects from '../../components/Projects/Projects';
-import TechStack from '../../components/TechStack/TechStack';
+import "./HomePage.scss";
+import MainHero from "../../components/MainHero/MainHero";
+import Projects from "../../components/Projects/Projects";
+import TechStack from "../../components/TechStack/TechStack";
 
 export default function HomePage(): JSX.Element {
 	return (
-		<section className='home-page'>
+		<section className="home-page">
 			<MainHero />
 			<TechStack />
 			<Projects />
