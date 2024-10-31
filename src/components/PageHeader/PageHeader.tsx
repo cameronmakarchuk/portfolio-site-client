29e3c40c6efe3e058f278d0988962330a4bfdddd
Cameron Makarchuk <cmakarchuk@gmail.com>
2024-10-30 22:32:13 -0400

    feat: dynamically add blur to header, disable active

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,5 +1,5 @@
 import "./PageHeader.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import darkToggleIcon from "../../assets/icons/dark_mode_FILL0_wght400_GRAD0_opsz48.svg";
 // import _lightToggleIcon from "../../assets/icons/light_mode_FILL0_wght400_GRAD0_opsz48.svg";
 import { NavLink, useLocation } from "react-router-dom";
@@ -7,10 +7,28 @@ import { NavLink, useLocation } from "react-router-dom";
 export default function PageHeader(): JSX.Element {
 	const location = useLocation();
 	const [active, setActive] = useState<string>(location.pathname);
+	const [blur, setBlur] = useState<boolean>(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			setBlur(window.scrollY > 0);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
 	return (
 		<header className="page-header">
 			<nav className="page-header__nav">
-				<ul className="page-header__nav-list">
+				<ul
+					className={
+						blur
+							? "page-header__nav-list page-header__nav-list--blur"
+							: "page-header__nav-list"
+					}
+				>
 					<NavLink
 						to="/"
 						className="page-header__nav-link"
