7495d58ef9b996256fa338413b733a7dd1c40496
Cameron Makarchuk <cmakarchuk@gmail.com>
2025-12-04 20:08:12 -0500

    chore: remove divs from app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,3 @@
-// import './App.scss';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import CallToAction from './components/CallToAction/CallToAction';
-// import PageHeader from './components/Header/Header';
-// import ProjectBrainStorm from './components/Projects/BrainStorm/BrainStorm';
-// import ProjectRecLeague from './components/Projects/RecLeague/RecLeague';
-// import AboutPage from './pages/About/About';
-// import ContactPage from './pages/Contact/Contact';
-// import HomePage from './pages/Home/Home';
-// import ProjectPage from './pages/Projects/Projects';
-
-// function App() {
-// 	return (
-// 		<BrowserRouter>
-// 			<PageHeader />
-// 			<Routes>
-// 				<Route path='/' element={<HomePage />} />
-//
-// 				<Route path='/projects' element={<ProjectPage />}>
-// 					<Route path='/projects/recleague' element={<ProjectRecLeague />} />
-// 					<Route path='/projects/brainstorm' element={<ProjectBrainStorm />} />
-// 				</Route>
-//
-// 				<Route path='/about' element={<AboutPage />} />
-// 				<Route path='/contact' element={<ContactPage />} />
-// 			</Routes>
-// 			<CallToAction />
-// 		</BrowserRouter>
-// 	);
-// }
-
 import './styles/global.scss';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
@@ -40,11 +9,6 @@ import Navigation from './components/Navigation/Navigation';
 function App() {
 	return (
 		<div>
-			{/* <div className='min-h-screen bg-slate-900 relative overflow-x-hidden'> */}
-			{/* 	<div className='fixed inset-0 bg-gradient-to-br from-pink-500/10 via-purple-500/10 to-cyan-500/10 pointer-events-none' /> */}
-			{/* 	<div className='fixed inset-0 bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-pink-900/20 via-transparent to-transparent pointer-events-none' /> */}
-			{/* 	<div className='fixed inset-0 bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-cyan-900/20 via-transparent to-transparent pointer-events-none' /> */}
-			{/**/}
 			<Navigation />
 			<Hero />
 			<About />
